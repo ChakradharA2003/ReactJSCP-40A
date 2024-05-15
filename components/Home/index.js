eac15903e87163b1aaca13017d836ef91fd8c88b
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-05-15 13:52:18 +0530

    Update index.js

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,5 +1,18 @@
 // Write your code here
 import './index.css'
 
-const Home = () => <div className="home-container">.</div>
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+      className="image1"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+      className="image2"
+    />
+  </div>
+)
 export default Home
