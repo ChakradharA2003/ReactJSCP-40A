66fd5894f1d731bee882ebcef4e9de5d8d9b9016
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-05-15 13:51:19 +0530

    Update index.js

--- components/About/index.js
+++ components/About/index.js
@@ -1,5 +1,18 @@
 // Write your code here
 import './index.css'
 
-const About = () => <div className="about-container">.</div>
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+      alt="about"
+      className="image1"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+      alt="about"
+      className="image2"
+    />
+  </div>
+)
 export default About
